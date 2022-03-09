938dad3aebf0e61b92c5814e59d263ade3b2163f
Jaume <ballestertomasj@gmail.com>
2022-03-09 17:26:07 +0100

    first

---
+++ forms/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <form>
+      <div>
+        <span>lala</span>
+        <input name="campo" />
+      </div>
+      <input name="campo-2" />
+      <input type="submit" value="Enviar" />
+    </form>
+  );
+}
+
+export default App;
