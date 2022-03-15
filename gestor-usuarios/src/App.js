6f2f37d8eaa398aa6eddf263ae2b20589cf5443d
Jaume <ballestertomasj@gmail.com>
2022-03-15 17:03:25 +0100

    Update App.js

--- gestor-usuarios/src/App.js
+++ gestor-usuarios/src/App.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 function App() {
+  const [formulario, setFormulario] = useState({ name: "" });
+  const handleChange = (e) => {
+    setFormulario({
+      ...formulario,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <form>
-      <input name="name" placeholder="Nombre" />
+      <input name="name" placeholder="Nombre" value={formulario.name} />
     </form>
   );
 }
