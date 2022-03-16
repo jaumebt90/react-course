11ce73a0216d21564b42d5c110698cca0a8f611d
Jaume <ballestertomasj@gmail.com>
2022-03-16 13:39:45 +0100

    container

--- gestor-usuarios/src/App.js
+++ gestor-usuarios/src/App.js
@@ -1,27 +1,30 @@
 import useFormulario from "./hooks/useFormulario";
 import Input from "./components/Input";
 import Card from "./components/Card";
+import Container from "./components/Container";
 
 function App() {
   const [formulario, handleChange] = useFormulario({ name: "", lastname: "" });
   console.log(formulario);
   return (
-    <Card>
-      <form>
-        <Input
-          label="Nombre"
-          name="name"
-          value={formulario.name}
-          onChange={handleChange}
-        />
-        <Input
-          label="Apellido"
-          name="lastname"
-          value={formulario.lastname}
-          onChange={handleChange}
-        />
-      </form>
-    </Card>
+    <Container>
+      <Card>
+        <form>
+          <Input
+            label="Nombre"
+            name="name"
+            value={formulario.name}
+            onChange={handleChange}
+          />
+          <Input
+            label="Apellido"
+            name="lastname"
+            value={formulario.lastname}
+            onChange={handleChange}
+          />
+        </form>
+      </Card>
+    </Container>
   );
 }
 
