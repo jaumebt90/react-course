bcdb8893c270d44132125202cf60176537f425fa
Jaume <ballestertomasj@gmail.com>
2022-03-16 13:34:49 +0100

    Card

--- gestor-usuarios/src/App.js
+++ gestor-usuarios/src/App.js
@@ -1,24 +1,27 @@
 import useFormulario from "./hooks/useFormulario";
 import Input from "./components/Input";
+import Card from "./components/Card";
 
 function App() {
   const [formulario, handleChange] = useFormulario({ name: "", lastname: "" });
   console.log(formulario);
   return (
-    <form>
-      <Input
-        label="Nombre"
-        name="name"
-        value={formulario.name}
-        onChange={handleChange}
-      />
-      <Input
-        label="Apellido"
-        name="lastname"
-        value={formulario.lastname}
-        onChange={handleChange}
-      />
-    </form>
+    <Card>
+      <form>
+        <Input
+          label="Nombre"
+          name="name"
+          value={formulario.name}
+          onChange={handleChange}
+        />
+        <Input
+          label="Apellido"
+          name="lastname"
+          value={formulario.lastname}
+          onChange={handleChange}
+        />
+      </form>
+    </Card>
   );
 }
 
