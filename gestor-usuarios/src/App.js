b313be34e88c04cca181834e01966d1f26fa6b20
Jaume <ballestertomasj@gmail.com>
2022-03-16 13:41:52 +0100

    css

--- gestor-usuarios/src/App.js
+++ gestor-usuarios/src/App.js
@@ -9,20 +9,22 @@ function App() {
   return (
     <Container>
       <Card>
-        <form>
-          <Input
-            label="Nombre"
-            name="name"
-            value={formulario.name}
-            onChange={handleChange}
-          />
-          <Input
-            label="Apellido"
-            name="lastname"
-            value={formulario.lastname}
-            onChange={handleChange}
-          />
-        </form>
+        <div style={{ padding: 20 }}>
+          <form>
+            <Input
+              label="Nombre"
+              name="name"
+              value={formulario.name}
+              onChange={handleChange}
+            />
+            <Input
+              label="Apellido"
+              name="lastname"
+              value={formulario.lastname}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
       </Card>
     </Container>
   );
