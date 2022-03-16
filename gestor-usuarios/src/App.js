ece0d7abd33164857186bafe845fdb5cbec5f7ed
Jaume <ballestertomasj@gmail.com>
2022-03-16 18:17:00 +0100

    refactor

--- gestor-usuarios/src/App.js
+++ gestor-usuarios/src/App.js
@@ -1,52 +1,20 @@
 import { useState } from "react";
-import useFormulario from "./hooks/useFormulario";
-import Input from "./components/Input";
 import Card from "./components/Card";
 import Container from "./components/Container";
-import Button from "./components/Button";
+import UserForm from "./components/UserForm";
 
 function App() {
   const [usuarios, setUsuarios] = useState([]);
-  const [formulario, handleChange, reset] = useFormulario({
-    name: "",
-    email: "",
-    lastname: "",
-  });
-  const submit = (e) => {
-    e.preventDefault();
-    setUsuarios([...usuarios, formulario]);
-    reset();
+
+  const submit = (usuario) => {
+    setUsuarios([...usuarios, usuario]);
   };
   return (
     <div style={{ marginTop: "15%" }}>
       <Container>
         <Card>
-          <div style={{ padding: 20 }}>
-            <form onSubmit={submit}>
-              <Input
-                label="Nombre"
-                name="name"
-                value={formulario.name}
-                onChange={handleChange}
-                placeholder="Nombre"
-              />
-              <Input
-                label="Apellido"
-                name="lastname"
-                value={formulario.lastname}
-                onChange={handleChange}
-                placeholder="Apellido"
-              />
-              <Input
-                label="Correo"
-                name="email"
-                value={formulario.email}
-                onChange={handleChange}
-                placeholder="Correo"
-              />
-              <Button>Enviar</Button>
-            </form>
-          </div>
+          <div style={{ padding: 20 }}></div>
+          <UserForm submit={submit} />
         </Card>
         <Card>
           <ul>
