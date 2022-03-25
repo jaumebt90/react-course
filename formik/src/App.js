28b6102be909df5a0922b89f55df5302d10d2982
Jaume <ballestertomasj@gmail.com>
2022-03-25 10:25:47 +0100

    form

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,15 +1,38 @@
-import {useFormik} from 'formik'
+import { useFormik } from "formik";
 
 function App() {
-  const formik=useFormik({
-    initialValues:{
-      name:'',
-      lastname:'',
-      email:'',
-    }
-  })
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      lastname: "",
+      email: "",
+    },
+    onSubmit: (values) => console.log(values),
+  });
   return (
-    
+    <form onSubmit={formik.handleSubmit}>
+      <label>Nombre</label>
+      <input
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      <label>Nombre</label>
+      <input
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      <label>Nombre</label>
+      <input
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+    </form>
   );
 }
 
