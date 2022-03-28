b25842ce84e10266cf86e8f5ab74d17cc65a7b7f
Jaume <ballestertomasj@gmail.com>
2022-03-28 11:42:31 +0200

    api call

--- buscador-imagenes/src/App.js
+++ buscador-imagenes/src/App.js
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import { Formik, Form, Field } from "formik";
 import "./header.css";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  console.log({ photos });
   return (
     <div>
       <header>
         <Formik
           initialValues={{ search: "" }}
           onSubmit={async (values) => {
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+              {
+                headers: {
+                  Authorization:
+                    "Client-ID zUUaGFf7hwTAZhMttvBsvA3wn8P_VJ-wmr4ESAtI6VE",
+                },
+              }
+            );
+            const data = await response.json();
             //llamar api unsplash
-            console.log(values);
+            setPhotos(data.results);
           }}
         >
           <Form>
