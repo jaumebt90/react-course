c4ace61ff787d6a270a14a58318a1916b1da58cd
Jaume <ballestertomasj@gmail.com>
2022-03-28 11:16:45 +0200

    search bar

--- buscador-imagenes/src/App.js
+++ buscador-imagenes/src/App.js
@@ -1,8 +1,23 @@
-
+import { Formik, Form, Field } from "formik";
+import "./header.css";
 
 function App() {
   return (
-    
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: "" }}
+          onSubmit={async (values) => {
+            //llamar api unsplash
+            console.log(values);
+          }}
+        >
+          <Form>
+            <Field name="search" />
+          </Form>
+        </Formik>
+      </header>
+    </div>
   );
 }
 
