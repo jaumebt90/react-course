dc10912ce27ca6e7097fffef007171af014d3105
Jaume <ballestertomasj@gmail.com>
2022-03-30 11:54:09 +0200

    Update App.js

--- styled/src/App.js
+++ styled/src/App.js
@@ -8,11 +8,25 @@ const Content = styled.div`
   padding: 20px 25px;
 `;
 const Button = styled.button`
+  transition: box-shadow 0.2s ease;
   background-color: ${(props) => (props.primary ? "red" : "white")};
   color: ${(props) => (props.primary ? "white" : "red")};
   padding: 10px 15px;
   border: solid 2px red;
   border-radius: 4px;
+
+  &:hover {
+    box-shadow: 1px 2px 5px rgb(0, 0, 0, 0.3);
+  }
+  //Estils asignats class
+  &.secondary {
+    background-color: blue;
+    border: solid 2px blue;
+    color: white;
+  }
+  .info {
+    font-size: 28px;
+  }
 `;
 //Afegint estils a nes ja creats a Button
 const BlockButton = styled(Button)`
@@ -30,8 +44,11 @@ function App() {
   return (
     <Content>
       <P>Hola soy un parrafo!</P>
-      <Button>Enviar</Button>
+      <Button>
+        Enviar<p className="info">Info</p>
+      </Button>
       <Button primary>Enviar</Button>
+      <Button className="secondary">Enviar</Button>
       <BlockButton primary as="a" href="#">
         Enviar
       </BlockButton>
