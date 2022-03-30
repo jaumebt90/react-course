dc322ce2f33d94a043de6e474d97283ab8fe1a64
Jaume <ballestertomasj@gmail.com>
2022-03-30 11:38:52 +0200

    styles

--- styled/src/App.js
+++ styled/src/App.js
@@ -14,14 +14,23 @@ const Button = styled.button`
   border: solid 2px red;
   border-radius: 4px;
 `;
-function App() {
-  return (
-    <Content>
-      <P>Hola soy un parrafo!</P>
-      <Button>Enviar</Button>
-      <Button primary>Enviar</Button>
-    </Content>
-  );
-}
+//Afegint estils a nes ja creats a Button
+const BlockButton = styled(Button)`
+  width: 100%;
+  font-size: 24px;
+`;
+const Link = () =>
+  function App() {
+    return (
+      <Content>
+        <P>Hola soy un parrafo!</P>
+        <Button>Enviar</Button>
+        <Button primary>Enviar</Button>
+        <BlockButton primary as="a" href="#">
+          Enviar
+        </BlockButton>
+      </Content>
+    );
+  };
 
 export default App;
