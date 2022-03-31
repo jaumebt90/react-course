b24d4b6c32db4ec3c1a419a37d4bce6e90908a71
Jaume <ballestertomasj@gmail.com>
2022-03-31 11:19:50 +0200

    Update Input.js

--- interes-compuesto/src/components/Input.js
+++ interes-compuesto/src/components/Input.js
@@ -1,8 +1,21 @@
 import styled from "styled-components";
 import { useField } from "formik";
 
+const Control = styled.div`
+  margin-bottom: 20px;
+`;
+
+const Label = styled.label`
+  color: #000;
+  display: block;
+  margin-bottom: 5px;
+`;
 const Input = ({ label, ...props }) => {
-  return "lala";
+  return (
+    <Control>
+      <Label>{label}</Label>
+    </Control>
+  );
 };
 
 export default Input;
