a1420cf97815dcf87004803bfa17520396d203e6
Jaume <ballestertomasj@gmail.com>
2022-03-31 12:09:22 +0200

    currency

--- interes-compuesto/src/App.js
+++ interes-compuesto/src/App.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { Formik, Form } from "formik";
 import Input from "./components/Input";
 import Container from "./components/Container";
 import Section from "./components/Section";
 import Button from "./components/Button";
+import Balance from "./components/Balance";
 
 const compoundInterest = (deposit, contribution, years, rate) => {
   let total = deposit;
@@ -13,7 +15,15 @@ const compoundInterest = (deposit, contribution, years, rate) => {
   return Math.round(total);
 };
 
+const formatter = new Intl.NumberFormat("es-ES", {
+  style: "currency",
+  currency: "EUR",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
 function App() {
+  const [balance, setBalance] = useState("");
   const handleSubmit = ({ deposit, contribution, years, rate }) => {
     const val = compoundInterest(
       Number(deposit),
@@ -21,6 +31,7 @@ function App() {
       Number(years),
       Number(rate)
     );
+    setBalance(formatter.format(val));
   };
   return (
     <Container>
@@ -42,6 +53,7 @@ function App() {
             <Button>Calcular</Button>
           </Form>
         </Formik>
+        {balance !== " " ? <Balance>Balance final: {balance}</Balance> : null}
       </Section>
     </Container>
   );
