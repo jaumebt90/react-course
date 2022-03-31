64ea3a99565edf217cdcfd273a68e089f3314a5b
Jaume <ballestertomasj@gmail.com>
2022-03-31 11:23:14 +0200

    Update Input.js

--- interes-compuesto/src/components/Input.js
+++ interes-compuesto/src/components/Input.js
@@ -10,10 +10,20 @@ const Label = styled.label`
   display: block;
   margin-bottom: 5px;
 `;
+const MyInput = styled.input`
+  outline: none;
+  padding: 8px;
+  border: solid 1px #b1b3b5;
+  border-radius: 4px;
+  width: 100%;
+  margin-bottom: 5px;
+`;
 const Input = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
   return (
     <Control>
       <Label>{label}</Label>
+      <MyInput {...field} {...props} />
     </Control>
   );
 };
