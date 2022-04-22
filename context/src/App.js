f2146855633c56e6118fd1205d742a1991b3fedb
Jaume <ballestertomasj@gmail.com>
2022-04-22 11:40:47 +0200

    Update App.js

--- context/src/App.js
+++ context/src/App.js
@@ -1,6 +1,11 @@
 import { createContext, useContext } from "react";
 
 const Context = createContext("valor por defecto");
+
+const Provider = ({ children }) => {
+  return <Context.Provider>{children}</Context.Provider>;
+};
+
 function App() {
   return <div>Hola mundo</div>;
 }
