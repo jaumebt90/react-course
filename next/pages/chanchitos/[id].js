53b44908793a6785ca176811ad58c498885918a1
Jaume <ballestertomasj@gmail.com>
2022-04-26 12:00:44 +0200

    Update [id].js

---
+++ next/pages/chanchitos/[id].js
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+
+const ChanchitoDinamico = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <p>Chanchito dinamico</p>
+    </div>
+  );
+};
+
+export default ChanchitoDinamico;
